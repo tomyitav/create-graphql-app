f46f076a86965ec6ae2ac3d63ccdec085136a108
Tom <tomyitav@gmail.com>
2018-11-21 02:21:36 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# create-graphql-app
+
+Cli tool for bootstrapping production grade GraphQL server.
---
+++ src/create-graphql-app.ts
@@ -0,0 +1,18 @@
+// Import here Polyfills if needed. Recommended core-js (npm i -D core-js)
+// import "core-js/fn/array.find"
+// ...
+import * as program from 'commander'
+import allCommands from './commands/all-commands'
+
+program
+  .version("0.0.1")
+  .description("Cli tool for bootstrapping production grade GraphQL server");
+
+for(let command of allCommands) {
+  program
+    .command(command.getName())
+    .description(command.getDescription())
+    .action(command.getAction())
+}
+
+program.parse(process.argv);
