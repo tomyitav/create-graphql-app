86d291917e501bf6e218b25c29bcce82f67f18e5
Tom <tomyitav@gmail.com>
2018-12-24 22:18:36 +0200

    refactor: small change in service generation
    extracted service generation to outer function

--- src/commands/service/service.ts
+++ src/commands/service/service.ts
@@ -9,8 +9,17 @@ import {
 import * as path from 'path'
 import { startCase } from 'lodash'
 import logger from '../../utils/logger'
+import { TypescriptParser } from 'typescript-parser'
+
+type DefinitionFileType = 'injector' | 'context'
 
 export class Service extends AbstractCommand {
+  private parser: TypescriptParser
+  constructor() {
+    super()
+    this.parser = new TypescriptParser()
+  }
+
   public getName(): string {
     return 'service [service-path]'
   }
@@ -38,32 +47,34 @@ export class Service extends AbstractCommand {
 
   private async createServiceFile(servicePath: string) {
     try {
-      const pathToServicesDir = await locateFile(servicePattern, './', 'dir')
-      if (pathToServicesDir.length === 0) {
-        logger.warn(
-          'services directory was not found. please make sure you are in project root directory, and that services directory exists'
-        )
-        return
-      } else if (pathToServicesDir.length > 1) {
-        logger.warn(
-          'Found two services directories: ' +
-            pathToServicesDir.toString() +
-            '. Please make sure only one services directory exists.'
-        )
-        return
-      } else {
-        const joinedPathToService = path.join(pathToServicesDir[0], servicePath)
-        const serviceFileExists = await fileExists(joinedPathToService)
-        if (serviceFileExists) {
-          logger.warn('Service file already exists. Please select a different location')
-        } else {
-          logger.info('Generating service file...')
-          await writeToFile(joinedPathToService, this.getServiceFileContent(servicePath))
-          logger.info('Service file was added to- ' + joinedPathToService)
-        }
-      }
+      await this.writeServiceFile(servicePath)
     } catch (err) {
       logger.error(`Failed to generate service- ${err}`)
+    }
+  }
+
+  private async writeServiceFile(servicePath: string) {
+    const pathToServicesDir = await locateFile(servicePattern, './', 'dir')
+    if (pathToServicesDir.length === 0) {
+      logger.warn(
+        'services directory was not found. please make sure you are in project root directory, and that services directory exists'
+      )
+    } else if (pathToServicesDir.length > 1) {
+      logger.warn(
+        'Found two services directories: ' +
+          pathToServicesDir.toString() +
+          '. Please make sure only one services directory exists.'
+      )
+    } else {
+      const joinedPathToService = path.join(pathToServicesDir[0], servicePath)
+      const serviceFileExists = await fileExists(joinedPathToService)
+      if (serviceFileExists) {
+        logger.warn('Service file already exists. Please select a different location')
+      } else {
+        logger.info('Generating service file...')
+        await writeToFile(joinedPathToService, this.getServiceFileContent(servicePath))
+        logger.info('Service file was added to- ' + joinedPathToService)
+      }
     }
   }
 
@@ -74,5 +85,12 @@ export class Service extends AbstractCommand {
 
   private getServiceClassName(servicePath: string) {
     return startCase(path.parse(servicePath).base.split('.')[0]) + serviceFileNameSuffix
+  }
+
+  private modifyServiceDefinitionFile(
+    pathToService: string,
+    definitionFileType: DefinitionFileType
+  ) {
+    const fileToLocate = definitionFileType + '.ts'
   }
 }
